157818a390db3308ed400d7959286aabb9e4f5cc
AndGi0 <118888570+AndGi0@users.noreply.github.com>
2024-03-26 20:37:21 +0100

    Update scriptOspite.js

--- scriptOspite.js
+++ scriptOspite.js
@@ -1,4 +1,4 @@
 function button_press(value){
     if(value=="login") window.location.href="https://andgi0.github.io/dspgsheet/gestioneUtenti/login.html";
-    else window.location.href="gestioneUtenti/login.html";
+    else window.location.href="https://andgi0.github.io/dspgsheet/gestioneUtenti/signup.html";
 }
